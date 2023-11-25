a4ca74689a6202eb6b3b931630a9def13417b5a1
Leechee403 <Leechee403@Leechee403.com>
2023-11-25 18:40:13 +0800

    :construction: admin: order detail

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -2,6 +2,7 @@ package com.sky.controller.admin;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
 import com.sky.vo.OrderStatisticsVO;
+import com.sky.vo.OrderVO;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -47,5 +49,18 @@ public class OrderController {
         log.info("各个状态的订单数量统计");
         OrderStatisticsVO orderStatisticsVO = orderService.statistic();
         return Result.success(orderStatisticsVO);
+    }
+
+    /**
+     * 查询订单详情
+     * @param id
+     * @return
+     */
+    @GetMapping("/details/{id}")
+    @ApiOperation("查询订单详情")
+    public Result<OrderVO> detail(@PathVariable Long id) {
+        log.info("查询订单详情,{}", id);
+        OrderVO orderVO = orderService.detail(id);
+        return Result.success(orderVO);
     }
 }
